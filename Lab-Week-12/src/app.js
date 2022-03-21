164329107d1e4c138cbd8d7c2c5a62fda3159d4a
vycodin <kbyin2@gmail.com>
2022-03-21 11:02:33 -0700

    Scholarship function has been added and sucessfully implemented

--- Lab-Week-12/src/app.js
+++ Lab-Week-12/src/app.js
@@ -1,4 +1,27 @@
+//part 2
 function element_selector(css_selector){
     const select_element = document.querySelector(css_selector)
-    console.log(select_element)
+    return select_element
 }
+console.log(element_selector())
+//part 3
+const students = [
+    { name: "Tasha", grades: [ 80, 67, 75, 62 ] },
+    { name: "Tuvok", grades: [ 90, 88, 91, 92 ] },
+    { name: "Dax", grades: [ 88, 90, 89, 85 ] },
+    { name: "Neelix", grades: [ 60, 54, 48, 50 ] }
+  ]
+  
+  const studentsScholarship = []
+function check_stu_grade(){
+  for (const st of students) {
+      let total = st.grades.reduce((a,b) => a+b,0) / st.grades.length;
+    if(total >= 80){
+        const ob = {name: st.name, gradeAvg: total}
+        studentsScholarship.push(ob)
+    }
+  }
+  return studentsScholarship
+}
+
+console.log(check_stu_grade())
